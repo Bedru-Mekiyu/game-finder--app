fa2449a64559f810acd4d2f3230065239a2443e5
Bedru Mekiyu <bedru.mekiyu-ug@aau.edu.et>
2025-11-15 09:41:41 +0300

    create genric custom hooks

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,44 +1,12 @@
-import {useState,useEffect} from 'react'
-import apiClient from '../api-client';
-import { CanceledError } from 'axios';
+import useData from "./useData";
 
-interface Genre{
+export interface Genre{
   id:number;
   name:string;
 }
 
-interface FetchGanresResponse{
-  count:number;
-  results:Genre[];
-}
-const useGenres=()=>{
-       const [genres, setGenres] = useState<Genre[]>([]);
-          const [error, setError] = useState("");
-          const [isLoading ,setLoadig]=useState(false);
-          useEffect(() => {
-    
-            const controller= new AbortController()
-            setLoadig(true)
-            apiClient
-              .get<FetchGanresResponse>("/genres" ,{signal:controller.signal})
-              .then((res) => {setGenres(res.data.results);
-    
-                   setLoadig(false)
-              })
-              .catch((err) =>{
-                if(err instanceof CanceledError) return ; 
-                 setError(err.message)
-                 setLoadig(false)
-                });
-                    
-              return ()=>controller.abort();
-          },[]);
-           return {
-            genres,
-            error,
-            isLoading
-           }
-} 
+
+const useGenres=()=> useData<Genre>('/genres');
 export default useGenres;
 
 
